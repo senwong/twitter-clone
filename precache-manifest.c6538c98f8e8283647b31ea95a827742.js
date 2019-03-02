1b724eeb5a5f6c5a53c774a08ee4aadb67bc1588
WJ941 <wangshaosen666@gmail.com>
2019-03-02 11:51:47 +0800

    Updates

---
+++ precache-manifest.c6538c98f8e8283647b31ea95a827742.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e25b963c8734f24ffd5",
+    "url": "/twitter-imitation/static/js/runtime~main.4e25b963.js"
+  },
+  {
+    "revision": "5e00f1e4aff59098d2ad",
+    "url": "/twitter-imitation/static/js/main.5e00f1e4.chunk.js"
+  },
+  {
+    "revision": "54718034ed056adde775",
+    "url": "/twitter-imitation/static/js/2.54718034.chunk.js"
+  },
+  {
+    "revision": "d22b86503c474d7127ca07ce8eb928cf",
+    "url": "/twitter-imitation/index.html"
+  }
+];
