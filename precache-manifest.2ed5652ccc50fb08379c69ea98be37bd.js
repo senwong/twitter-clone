02dc24452f340c6747294c505516595e43b79b35
WJ941 <wangshaosen666@gmail.com>
2019-05-24 23:00:56 +0800

    Updates

---
+++ precache-manifest.2ed5652ccc50fb08379c69ea98be37bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "13e36d3211dc583488a5",
+    "url": "/twitter-clone/static/js/runtime~main.13e36d32.js"
+  },
+  {
+    "revision": "1a03883223c6ab071423",
+    "url": "/twitter-clone/static/js/main.1a038832.chunk.js"
+  },
+  {
+    "revision": "717ecdfb37e0bdc1b48b",
+    "url": "/twitter-clone/static/js/2.717ecdfb.chunk.js"
+  },
+  {
+    "revision": "dd9f48ffef50d26fc864a8d5fda0c33b",
+    "url": "/twitter-clone/index.html"
+  }
+];
